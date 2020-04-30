72b451249f22e0243ec9dd82379b75db31416813
Franky Ang <frankyang53@gmail.com>
2020-04-30 16:52:31 +0700

    Add sub menu, add form with button, prevent data leaks, check woocommerce plugin

--- export-data.php
+++ export-data.php
@@ -8,15 +8,48 @@ Author: Group G (Reggie Maurice Gunawan, Frengki Ang)
 Author URI: http://grupg-pbwlanjut.com/
 */
 
+//to prevent data leaks, so it cant be accessed directly
+if(!defined('ABSPATH'))
+{
+    exit; 
+}
+
+//check if woocommerce plugin is active
+if(!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_options('active_plugins'))))
+{
+    exit;
+}
+
 add_action('admin_menu', 'add_export_menu');
 
 function add_export_menu()
 {
-    add_menu_page('Export Order', 'Export Order', 'manage_options', 'export-order', 'export_menu_output', 'dashicons-media-spreadsheet');
+    $hook_menu = add_submenu_page('woocommerce', 'Export Order', 'Export Order', 'view_woocommerce_reports', 'export-order', 'export_menu_output');
 }
 
-function export_menu_output(){
-    echo '<div><h2>Export File to CSV or XLS</h2>Silahkan gunakan fitur export file ke dalam bentuk CSV atau XLS</div>';
+function export_menu_output()
+{
+    if(!current_user_can('view_woocommerce_reports'))
+    {
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h2>Export File to CSV or XLS</h2>
+        Silahkan gunakan fitur export file ke dalam bentuk CSV atau XLS
+        <form action="<?php menu_page_url('export-order') ?>" method="post">
+            <button type="submit" class="button">Export now</button>
+        </form>
+
+        
+    </div>
+
+    <?php
+        add_action('load-',$hook_menu,'export_order_submit');
+        function export_order_submit()
+        {
+
+        }
+    ?>
 }
 
-?>
