e513caad9497739a11e776372e56a72396ab9e35
reggie <reggiegunawan88@gmail.com>
2020-04-30 21:14:10 +0700

    Button export function

--- export-data.php
+++ export-data.php
@@ -1,22 +1,31 @@
-<?php
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">
 
+<?php
 /*
 Plugin Name: Export CSV/XLS Plugin
-Description: Plugin untuk melakukan export database kedalam bentuk file CSV/XLS.
+Description: Plugin sederhana untuk melakukan export data ke dalam bentuk file CSV/XLS.
 Version: 1.0.0
 Author: Group G (Reggie Maurice Gunawan, Frengki Ang)
 Author URI: http://grupg-pbwlanjut.com/
 */
 
 //to prevent data leaks, so it cant be accessed directly
-if(!defined('ABSPATH'))
-{
-    exit; 
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 //check if woocommerce plugin is active
-if(!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins'))))
-{
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
     exit;
 }
 
@@ -27,26 +36,58 @@ function add_export_menu()
     $hook_export_order = add_submenu_page('woocommerce', 'Export Order', 'Export Order', 'view_woocommerce_reports', 'export-order', 'export_menu_output');
 }
 
-function export_menu_output()
-{
-    if(!current_user_can('view_woocommerce_reports'))
-    {
-        return;
-    }
-    
-    ?>
-    <div class='wrap'>
-        <h2>Export File to CSV or XLS</h2>
-        Silahkan gunakan fitur export file ke dalam bentuk CSV atau XLS
-        <form action="<?php menu_page_url('export-order')?>"" method="post">
-            <button type='submit' class='button'>Export now</button>
-        </form>
-    </div>
-    <?php
+function export_menu_output(){
+echo '<div class="wrap center"><h2>Export File to CSV/XLS</h2>';
+echo '<h3>Silahkan pilih fitur dibawah ini untuk melakukan export data ke dalam bentuk CSV/XLS</h3></div>';
+ 
+echo '<table id="example" class="display" style="width:100%">';
+echo
+ '<thead>
+     <tr>
+     <th>Name</th>
+     <th>Position</th>
+     <th>Office</th>
+     <th>Age</th>
+     <th>Start date</th>
+     <th>Salary</th>
+     </tr>
+ </thead>';
+ echo 
+ '<tbody>
+ <tr>
+             <td>Tiger Nixon</td>
+             <td>System Architect</td>
+             <td>Edinburgh</td>
+             <td>61</td>
+             <td>2011/04/25</td>
+             <td>$320,800</td>
+         </tr>
+         <tr>
+             <td>Garrett Winters</td>
+             <td>Accountant</td>
+             <td>Tokyo</td>
+             <td>63</td>
+             <td>2011/07/25</td>
+             <td>$170,750</td>
+         </tr>
+ </tbody>';
+ echo '</table>';
 }
+?>
 
-add_action('load-'.$hook_export_order,'export_order_submit');
-function export_order_submit()
-{
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'excelHtml5',
+            'csvHtml5',
+        ]
+    } );
+} );
+</script>
 
-}
+
+
+
+
